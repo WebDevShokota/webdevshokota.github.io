ec7afe9790e50902b700c6415dc5e5432c8e662c
Daniil Shokota <40712643+WebDevShokota@users.noreply.github.com>
2018-07-05 15:11:37 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -5,20 +5,21 @@ $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
 $name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
 $email = $_POST['user_email'];
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'urj4j3u3uurr@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '$dk820&123&jekwl'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'dzharuzov@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('urj4j3u3uurr@yandex.ru'); // от кого будет уходить письмо?
-$mail->addAddress('MAGNUM13L@yandex.ru');     // Кому будет уходить письмо 
+$mail->setFrom('dzharuzov@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('gomudusu@p33.org');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
